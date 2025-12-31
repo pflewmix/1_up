a2a4bc1769fe1dcf5210cee46e776f9c315357cb
pflewmix <116694498+pflewmix@users.noreply.github.com>
2025-12-30 17:12:28 -0800

    updated rps js code. game now completes a full round with output of results

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -8,7 +8,7 @@ function getComputerChoice() {
         return 'rock';
     }
 }
-//console.log(getComputerChoice());
+console.log(`Computer: ${getComputerChoice()}`);
 
 let human = prompt('Enter rock, paper, or scissors.').toLowerCase(0);
 function getHumanChoice() {
@@ -22,13 +22,34 @@ function getHumanChoice() {
         return 'Invalid Entry: Please enter rock, paper, or scissors!'
     }
 }
-//console.log(getHumanChoice());
+console.log(`Me: ${getHumanChoice()}`);
 
 let humanScore = 0;
 let computerScore = 0;
 let humanChoice = getHumanChoice();
 let computerChoice = getComputerChoice();
 
-function playRound() {
-    
+function playRound(humanSelect, computerSelect) {
+    if (humanSelect == 'rock' || humanSelect == 'paper' || humanSelect == 'scissors') {
+        if (humanSelect === computerSelect) {
+            console.log('Draw: No score!');
+        } else if (humanSelect == 'scissors' && computerSelect == 'paper') {
+            console.log(`You win! ${humanSelect} beats ${computerSelect}.`);
+            humanScore++
+        } else if (humanSelect == 'rock' && computerSelect == 'scissors') {
+            console.log(`You win! ${humanSelect} beats ${computerSelect}.`);
+            humanScore++
+        } else if (humanSelect == 'paper' && computerSelect == 'rock') {
+            console.log(`You win! ${humanSelect} beats ${computerSelect}.`);
+            humanScore++
+        } else {
+            console.log(`You lose! ${computerSelect} beats ${humanSelect}.`);
+            computerScore++
+        }
+        console.log(`You: ${humanScore} | CPU: ${computerScore}`);
+    } else {
+        console.log('Invalid Entry');
+    }
 }
+
+playRound(humanChoice, computerChoice);
