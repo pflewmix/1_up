d47a1051429c084f4babbada50b1ae5c17645019
pflewmix <116694498+pflewmix@users.noreply.github.com>
2025-12-31 13:14:50 -0800

    completed code for funciton of rock paper scissors game.

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -1,3 +1,4 @@
+// gets and assigns computer choice to function
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
     if (num == 2) {
@@ -9,7 +10,7 @@ function getComputerChoice() {
     }
 }
 
-
+// gets and assigns user choice to function
 function getHumanChoice() {
     let input = prompt('Enter rock, paper, or scissors').toLowerCase();
     if (input === 'rock' || input === 'scissors' || input === 'paper') {
@@ -18,13 +19,17 @@ function getHumanChoice() {
         return null;
     }
 }
-//c
 
+// declares variables and initial amounts for scores
 let humanScore = 0;
 let computerScore = 0;
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
 
+// assigns function return value to variable
+let humanChoice
+// assigns function return value to variable
+let computerChoice
+
+// code for completing one round
 function playRound(humanSelect, computerSelect) {
     console.log(`Me: ${humanSelect}`);
     console.log(`Computer: ${computerSelect}`);
@@ -43,12 +48,27 @@ function playRound(humanSelect, computerSelect) {
         console.log(`You lose! ${computerSelect} beats ${humanSelect}.`);
         computerScore++
     }
-    console.log(`You: ${humanScore} | CPU: ${computerScore}`);
-    
+    console.log(`You: ${humanScore} | CPU: ${computerScore}`);    
 }
 
-if (humanChoice === null) {
-    console.log('Invalid entry. Round not played.');
-} else {
-    playRound(humanChoice, computerChoice);
+function playGame() {
+    for (i = 0; i < 5; i++) {
+        humanChoice = getHumanChoice();
+        if (humanChoice === null) {
+            console.log('Invalid entry. Round not played.');
+            i--;
+        } else {
+            computerChoice = getComputerChoice();
+            playRound(humanChoice, computerChoice);
+        }
+    }
+    if (humanScore > computerScore) {
+        console.log('You win!');
+    } else if (humanScore == computerScore) {
+        console.log('Draw! Play again!');
+    } else {
+        console.log('Computer wins!');
+    }
 }
+
+playGame();
