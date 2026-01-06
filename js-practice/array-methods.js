0a1bcc902ded076ac936aabebab21b473a046e7a
pflewmix <116694498+pflewmix@users.noreply.github.com>
2026-01-06 11:12:52 -0800

    notes on array methods in js-practcie

---
+++ js-practice/array-methods.js
@@ -0,0 +1,46 @@
+let arr = [];
+arr.push('Ball'); // adds items to the end
+arr.pop(); // extracts an item from the end
+arr.shift(); // extracts an item from the beginning
+arr.unshift('I', 'study', 'Javascript'); // adds items to the beginning
+
+console.log(arr);
+
+// splice()
+let replace = arr.splice(1, 1,); // removes based off index position (1st value), and the number of elements to remove from that point (2nd value)
+// the above stores the spliced elements in variable 'replace'
+console.log(replace);
+
+arr.splice(1, 0, 'study', 'and', 'code'); // values can also be added without replacement by using 0 for the 2nd value
+console.log(arr);
+
+// using negative indexes
+let numsArray = [1, 2, 5];
+numsArray.splice(-1, 0, 3, 4); // negative indexes can also be used to determine the index being spliced
+
+console.log(numsArray);
+
+// slice() - returns a new array with all items between the specified start and end
+let letters = ['t', 'e', 's', 't'];
+console.log(letters.slice(1, 3)); // returns 'e, s'
+console.log(letters.slice(-2)); // returns 's, t'
+
+// concat() - creates a new array that includes values from other arrays and additional items
+let nums = [1, 2];
+console.log(nums.concat([3, 4])); // 1,2,3,4
+console.log(nums.concat([3, 4], [5, 6])); // 1,2,3,4,5,6
+console.log(nums.concat([3, 4], 5, 6)); 1,2,3,4,5,6
+
+let arrayLike = {
+    0: 'something',
+    1: 'else',
+    [Symbol.isConcatSpreadable]: true, // without this special property, objects that aren't arrays can't extract their elements 
+    length: 2
+};
+
+console.log(nums.concat(arrayLike)); // 1,2,something, else
+
+// forEach() - allows to run a function for every element of an array
+['Bilbo', 'Gandalf', 'Nazgul'].forEach((item, index, array) => {
+    console.log(`${item} is at index ${index} in ${array}`);
+});
